4b73764d90da5236aca3d2b2f8bbd7da011594e6
matovu-ronald <maronwhite6@gmail.com>
2018-06-09 22:46:15 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bootstrap 4 Starter Packer
+### Version 4.1.1
+    1. Clone the repository
+    2. Run nmp install, in the project folder to install all the dependecies
+    3. Then run npm start to start the project at port:     http://localhost:3000/
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+// Compile Sass & Inject into Browser
+gulp.task('sass', function(){
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+// Move js files to src/js
+gulp.task('js', function(){
+    return gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js'
+    ])
+        .pipe(gulp.dest("src/js"))
+        .pipe(browserSync.stream());
+
+});
+
+// Watch Sass and Serve
+gulp.task('serve', ['sass'], function(){
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+
+});
+
+// Move fonts folder to src
+gulp.task('fonts', function(){
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        pipe(gulp.dest("src/fonts"));
+});
+
+// Move font-awesome css to folder to src/css
+gulp.task('fa', function(){
+    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+    pipe(gulp.dest("src/css"));
+});
+
+
+// Default Gulp Task to run when we execute the gulp command
+gulp.task('default', ['sass', 'js', 'serve', 'fonts', 'fa']);
